edf7f8dcf8d31fdc4ffd22daa1b79e11089712a5
Hesam Alavi <hesamalavi@Hesams-MacBook-Pro.local>
2020-05-14 14:08:14 +1000

    Redux

--- src/playground/redux-101.js
+++ src/playground/redux-101.js
@@ -1,7 +1,43 @@
 import { createStore } from 'redux';
 
-const store = createStore((state = { count: 0 }) => {
-  return state;
+const store = createStore((state = { count: 0 }, action) => {
+    switch (action.type) {
+        case 'INCREMENT':
+            const incrementBy =
+                typeof action.incrementBy === 'number' ? action.incrementBy : 1;
+            return {
+                count: state.count + incrementBy,
+            };
+        case 'DECREMENT':
+            return {
+                count: state.count - 1,
+            };
+
+        case 'RESET':
+            return {
+                count: 0,
+            };
+        default:
+            return state;
+    }
 });
 
-console.log(store.getState());
+store.subscribe(() => {
+    console.log(store.getState());
+});
+
+store.dispatch({
+    type: 'INCREMENT',
+    incrementBy: 53,
+});
+store.dispatch({
+    type: 'INCREMENT',
+});
+
+store.dispatch({
+    type: 'RESET',
+});
+
+store.dispatch({
+    type: 'DECREMENT',
+});
